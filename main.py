8a744129a0e4c573e9efc656be067d54cc30f888
dcw32 <dcw32@cam.ac.uk>
2015-08-07 14:57:27 +0100

    Replace D1, D3 with dictionaries

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from optical_depth import od_chems
 from j_rates import j
 from k_rates import k
 from steady_state import steady,ozone,otp
-from d_arrays import d1_init,d2_calc,d3_calc
+from d_arrays import d1_init,d2_calc_new,d3_calc_new
 from plotting import altconc,linoxyoz,logoxyoz
 
 #Running options
@@ -25,10 +25,10 @@ ratio = 0.21
 # Steady state mode - can set up 'initial values' for chem scheme
 steadystate = True
 interactive = False
-numerics = False
-new_numerics = True
+numerics = True
+new_numerics = False
 # Plotting scripts - plots oxygen vs ozone for a range of oxygen vals
-plot_on = True
+plot_on = False
 # Chemical scheme: ChapmanSS, Chapman
 chem_scheme = 'Chapman'
 
@@ -63,6 +63,10 @@ if steadystate==True:
 	print "CALCULATING STEADY STATE OZONE COLUMN"
 	height,o3,o2,o,J_o2,J_o3,o3_running=steady(nlevs,h_max,h_min,H,M_surf,ratio,o2_c,o3_c,T,sol,sol_bin_width)
 	ncsave(heights,'O3',o3)
+	ncsave(heights,'O',o)
+	ncsave(heights,'O2',o2)
+	M=o2/ratio
+	ncsave(heights,'M',M)
 	altconc(height,o3,o3_running)
 
 #Interactive plot of ozone vs o2
@@ -92,7 +96,6 @@ if steadystate==True:
 			l.set_ydata(o3/1E12)
 			n.set_ydata(J_o3*1E3)
 			du=o3_running/2.69E16
-			print du
 	                #ax0.annotate(str(int(round(du))), xy=(0.99,0.01),xycoords='axes fraction',horizontalalignment='right', verticalalignment='bottom')
 			fig.canvas.draw_idle()
 		so2.on_changed(update)
@@ -108,7 +111,6 @@ if plot_on==True:
 if new_numerics==True:
         file=Dataset('netcdf/O3.nc')
         o3_init=file.variables['O3'][:]
-	print o3_init
         file=Dataset('netcdf/O2.nc')
         o2=file.variables['O2'][:]
 	M=o2/ratio
@@ -136,16 +138,14 @@ if new_numerics==True:
 		o0=o_init[i]
 		o30=o3_init[i]
 		y0=[o30,o0]
-		t=np.linspace(0,3.1104E8,3600)
+		t=np.linspace(0,252E6,3600)
 		soln=odeint(f,y0,t)
 		soln=soln[len(t)-1,:]
 		o3[i]=soln[0]
 		o[i]=soln[1]
 		o3_running=o3_running+o3[i]*(1E5*(h_max-h_min)/(nlevs-1))
 		o2_running=o2_running+o2[i]*(1E5*(h_max-h_min)/(nlevs-1))
-	print o3
-	plt.plot(heights,o3_init)
-	plt.plot(heights,o3)
+	plt.plot(heights,(o3_init-o3)/o3_init)
 	plt.savefig('new_numerics.png')
 if numerics==True:
 #The D1 array contains the chemical species concentrations
@@ -153,28 +153,30 @@ if numerics==True:
 
 	print "CONSTRUCTING D1 ARRAY"
 	d1defs=np.genfromtxt("species.dat",dtype='str',skiprows=2)
-	d1=d1_init(d1defs,nlevs,ratio)
+	#d1=d1_init(d1defs,nlevs,ratio)
+	d1={}
+	k=0
+	while k<len(d1defs[:,0]):
+	        D=Dataset('netcdf/'+d1defs[k,0]+'.nc')
+	        value=D.variables[d1defs[k,0]][:]
+	        key=d1defs[k,0]
+	        d1[key]=value
+	        k+=1
 	bimol=np.genfromtxt("bimol.dat",dtype='str',skiprows=2)
 	photo=np.genfromtxt("photol.dat",dtype='str',skiprows=2)
 	nrxns=len(bimol)+len(photo)
 #The D2 array contains the chemical tendencies for each reaction
 #There is a chemical tendency arising from each chemical reaction
-	for i in range(100):
-		d2=d2_calc(nrxns,nlevs,d1,bimol,T,photo,o2_c,o3_c,sol,sol_bin_width,d1defs,d1[np.where(d1defs=='M')[0][0],:],h_max,h_min)
-		d3=d3_calc(d1defs,nlevs,bimol,photo,d2)
+	for i in range(1):
+		d2=d2_calc_new(nrxns,nlevs,d1,bimol,T,photo,o2_c,o3_c,sol,sol_bin_width,d1defs,d1['M'],h_max,h_min)
+		d3=d3_calc_new(d1defs,nlevs,bimol,photo,d2)
 		#print d3[1,:]
 		for i in range(len(d1defs[:,0])):
-			for j in range(nlevs):
-				d1[i,j]=d1[i,j]+d3[i,j]
-				if d1[i,j]<0:
-					d1[i,j]=0
-		print d1
-		doz=d1[1,:]
+			d1[d1defs[i,0]]=d1[d1defs[i,0]]+d3[d1defs[i,0]]
+		doz=d1['O3']
 		doz=doz*(1E5*(h_max-h_min)/(nlevs-1))
 		doz=np.sum(doz)
 		doz=doz/2.69E16
-		print doz
 du=o3_running/2.69E16
-print str(du)+" DU Ozone Column"
 #plt.plot(np.linspace(h_min,h_max,nlevs),d1[1,:])
 #plt.show()
--- optical_depth.py
+++ optical_depth.py
@@ -11,3 +11,15 @@ def od_chems(d1,d1defs,o2_c,o3_c,sol,nlevs,wbins,h_max,h_min):
 	I_factor=np.exp(-opd)
 	I=I_factor*sol
 	return I_factor
+def od_chems_new(d1,d1defs,o2_c,o3_c,sol,nlevs,wbins,h_max,h_min):
+        o2col=np.zeros([nlevs,wbins])
+        o3col=np.zeros([nlevs,wbins])
+        for i in range(nlevs):
+                for j in range(wbins):
+                        o2col[i,j]=(i+1)*(1E5*(h_max-h_min)/(nlevs-1))*np.sum(d1['O2'][:i+1])
+                        o3col[i,j]=(i+1)*(1E5*(h_max-h_min)/(nlevs-1))*np.sum(d1['O3'][:i+1])
+        opd=o2col*o2_c+o3col*o3_c
+        I_factor=np.exp(-opd)
+        I=I_factor*sol
+        return I_factor
+
