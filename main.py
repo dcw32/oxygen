27c61b1e52ab3f599f5c17caadc7a1bb307d99d8
dcw32 <dcw32@cam.ac.uk>
2015-08-13 12:35:20 +0100

    Revert to arrays for D1 etc. Adjustments to chemical solver code

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ from netCDF4 import Dataset
 from scipy.integrate import odeint,ode
 
 from data import extract_csec,ncsave
-from optical_depth import od_chems_new
+from optical_depth import od_chems_new,od_chems
 from j_rates import j
 from k_rates import k
 from steady_state import steady,ozone,otp
-from d_arrays import d2_calc_new,d3_calc_new,d2_calc,d3_calc
+from d_arrays import d2_calc,d3_calc,d1_init
 from plotting import altconc,linoxyoz,logoxyoz
 
 #Running options
@@ -25,7 +25,6 @@ ratio = 0.21
 # Steady state mode - can set up 'initial values' for chem scheme
 steadystate = True
 interactive = False
-numerics = False
 new_numerics = True
 # Plotting scripts - plots oxygen vs ozone for a range of oxygen vals
 plot_on = True
@@ -34,7 +33,7 @@ plot_on = True
 
 #Constants
 #Number of atmospheric levels
-nlevs=161
+nlevs=81
 #Height, km
 h_min=0
 h_max=80
@@ -43,6 +42,8 @@ heights=np.linspace(h_max,h_min,nlevs)
 H=7
 #surface M, cm-3
 M_surf=2.5E19*(ratio+0.79)
+
+box_h=(1E5*(h_max-h_min)/(nlevs-1))
 
 # wavelength bins, interpolating o2 & solar onto o3 csec wavelengths, extract temps
 sol_bin_width,o2_c,o3_c,sol,T=extract_csec(heights)
@@ -89,98 +90,45 @@ if plot_on==True:
 	linoxyoz(nlevs,h_max,h_min,H,o2_c,o3_c,T,sol,sol_bin_width)
 
 if new_numerics==True:
-        file=Dataset('netcdf/O3.nc')
-        o3_init=file.variables['O3'][:]
-        file=Dataset('netcdf/O2.nc')
-        o2=file.variables['O2'][:]
-	M=o2/ratio
-        file=Dataset('netcdf/O.nc')
-        o_init=file.variables['O'][:]
 	o2_running=0
 	o3_running=0
 	o3=np.zeros(nlevs)
 	o=np.zeros(nlevs)
+        d1defs=np.genfromtxt("species.dat",dtype='str',skiprows=2)
+        bimol=np.genfromtxt("bimol.dat",dtype='str',skiprows=2)
+        photo=np.genfromtxt("photol.dat",dtype='str',skiprows=2)
+        nrxns=len(bimol)+len(photo)
+        nspec=len(d1defs[:,0])
+	d1=d1_init(d1defs,nlevs,ratio)
+	o2=d1[np.where(d1defs=='O2')[0][0],:]
+	M=d1[np.where(d1defs=='M')[0][0],:]
+        d3=np.zeros([len(d1defs[:,0]),nlevs])
 	for i in range(nlevs):
-		k3M=k('k3M',T[i],M[i])
-		k4=k('k4',T[i],M[i])
-		optical_depth=o2_c*o2_running+o3_c*o3_running
-		I_factor=np.exp(-optical_depth)
-		I=np.array(sol*I_factor)
-		J2=j(I,o2_c,o3_c,'JO2',sol_bin_width)
-		J3=j(I,o2_c,o3_c,'JO3',sol_bin_width)
+		#Create dictionary of rates
+                rates={}
+                for a in range(len(bimol)):
+                        rates[a]=k(bimol[a,2],T[i],M[i])
+                I=od_chems(o2_running,o3_running,o2_c,o3_c,sol)
+                for a in range(len(photo)):
+                        rates[a+len(bimol)]=j(I,o2_c,o3_c,photo[a,1],sol_bin_width)
+		#Define function for chemical tendencies
 		def f(y, t):
-			g=[k3M*y[1]*o2[i]-(J3+k4*y[1])*y[0],2*J2*o2[i]+J3*y[0]-(k3M*o2[i]+k4*y[0])*y[1]]
+			g=[rates[0]*y[1]*o2[i]-(rates[3]+rates[1]*y[1])*y[0],2*rates[2]*o2[i]+rates[3]*y[0]-(rates[0]*o2[i]+rates[1]*y[0])*y[1]]
 			return g
 		#initial conditions
-		o0=o_init[i]
-		o30=o3_init[i]
-		y0=[o30,o0]
-		t=np.linspace(0,8640000,2400)
-		#There's probably a more efficient way to do this but meh
+		y0=[d1[np.where(d1defs=='O3')[0][0],i],d1[np.where(d1defs=='O')[0][0],i]]
+		time_tot=8640
+		t=np.linspace(0,time_tot,time_tot/3600)
+		#solution provides a time series, take the final value
 		soln=odeint(f,y0,t)
 		soln=soln[len(t)-1,:]
-		o3[i]=soln[0]
-		o[i]=soln[1]
-		o3_running=o3_running+o3[i]*(1E5*(h_max-h_min)/(nlevs-1))
-		o2_running=o2_running+o2[i]*(1E5*(h_max-h_min)/(nlevs-1))
-	plt.plot(heights,(o3_init-o3)/o3_init)
+		if i==0:
+			print soln
+		d3[np.where(d1defs=='O3')[0][0],i]=soln[0]
+                d3[np.where(d1defs=='O')[0][0],i]=soln[1]
+		o3_running=o3_running+d3[np.where(d1defs=='O3')[0][0],i]*box_h
+		o2_running=o2_running+d1[np.where(d1defs=='O2')[0][0],i]*box_h
+	#print d3[np.where(d1defs=='O3')[0][0],:]
+	#print o3_init
+	plt.plot(heights,(d1[np.where(d1defs=='O3')[0][0],:]-d3[np.where(d1defs=='O3')[0][0],:])/d1[np.where(d1defs=='O3')[0][0],:])
 	plt.savefig('new_numerics.png')
-if numerics==True:
-#The D1 array contains the chemical species concentrations
-#D1 metadata is contained in D1DEFS
-
-	print "CONSTRUCTING D1 ARRAY"
-	d1defs=np.genfromtxt("species.dat",dtype='str',skiprows=2)
-	#d1=d1_init(d1defs,nlevs,ratio)
-	d1={}
-	y=0
-	while y<len(d1defs[:,0]):
-	        D=Dataset('netcdf/'+d1defs[y,0]+'.nc')
-	        value=D.variables[d1defs[y,0]][:]
-	        key=d1defs[y,0]
-	        d1[key]=value
-	        y+=1
-	print d1.__class__
-	bimol=np.genfromtxt("bimol.dat",dtype='str',skiprows=2)
-	photo=np.genfromtxt("photol.dat",dtype='str',skiprows=2)
-	nrxns=len(bimol)+len(photo)
-	nspec=len(d1defs[:,0])
-#The D2 array contains the chemical tendencies for each reaction
-#There is a chemical tendency arising from each chemical reaction
-	for i in range(1):
-		rates={}
-		for a in range(len(bimol)):
-			rates[a]=k(bimol[a,2],T[i],M[i])
-		I=od_chems_new(d1,d1defs,o2_c,o3_c,sol,nlevs,len(sol),h_max,h_min)
-		for a in range(len(photo)):
-			rates[a+len(bimol)]=j(I,o2_c,o3_c,photo[a,1],sol_bin_width)
-		print rates
-		def f(y, t):
-			for a in range(nspec):
-				sp=d1defs[a,0]
-				d3[sp]=y[a]
-				d2[sp]=0
-				for b in range(np.where(bimol==d1defs[a,0])[:][0].shape[0]):
-					rxno=np.where(bimol==d1defs[i,0])[0][j]
-					if np.where(bimol==d1defs[a,0])[1][b]<2:
-						d2[sp]=d2[sp]-d3[bimol[rxno,0]]*d3[bimol[rxno,1]]*rates[bimol[rxno,2]]
-					else:
-						d2[sp]=d2[sp]+d3[bimol[rxno,0]]*d3[bimol[rxno,1]]*rates[bimol[rxno,2]]
-			return d2
-		t=[0,3600,7200]
-		soln=ode(f).set_integrator('lsoda')
-		print soln
-		soln.integrate(soln,3600)
-#		soln=odeint(f,d1,t)
-		#d2=d2_calc_new(nrxns,nlevs,d1,bimol,T,photo,o2_c,o3_c,sol,sol_bin_width,d1defs,d1['M'],h_max,h_min)
-		#d3=d3_calc_new(d1defs,nlevs,bimol,photo,d2)
-		#print d3[1,:]
-		#for i in range(len(d1defs[:,0])):
-		#	d1[d1defs[i,0]]=d1[d1defs[i,0]]+d3[d1defs[i,0]]
-		#doz=d1['O3']
-		#doz=doz*(1E5*(h_max-h_min)/(nlevs-1))
-		#doz=np.sum(doz)
-		#doz=doz/2.69E16
-du=o3_running/2.69E16
-#plt.plot(np.linspace(h_min,h_max,nlevs),d1[1,:])
-#plt.show()
