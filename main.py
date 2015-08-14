17dd3abceec65a50db63ba5b9b07d4e999bde68c
david <dcw32@cam.ac.uk>
2015-08-14 11:28:07 +0100

    Small changes to comments

--- main.py
+++ main.py
@@ -50,7 +50,8 @@ sol_bin_width,o2_c,o3_c,sol,T=extract_csec(heights)
 
 if steadystate==True:
 	print "CALCULATING STEADY STATE OZONE COLUMN"
-	height,o3,o2,o,J_o2,J_o3,o3_running=steady(nlevs,h_max,h_min,H,M_surf,ratio,o2_c,o3_c,T,sol,sol_bin_width,True)
+	height,o3,o2,o,J_o2,J_o3,o3_running=steady(nlevs,h_max,h_min,H,M_surf,ratio,o2_c,o3_c,T,\
+                                                   sol,sol_bin_width,True)
 	altconc(height,o3,o3_running)
 
 #Interactive plot of ozone vs o2
@@ -75,7 +76,8 @@ if steadystate==True:
 		def update(val):
 			ratio=so2.val
 			M_surf=2.5E19*(ratio+0.79)
-			height,o3,o2,o,J_o2,J_o3,o3_running=steady(nlevs,h_max,h_min,H,M_surf,ratio,o2_c,o3_c,T,sol,sol_bin_width,False)
+			height,o3,o2,o,J_o2,J_o3,o3_running=steady(nlevs,h_max,h_min,H,\
+                                               M_surf,ratio,o2_c,o3_c,T,sol,sol_bin_width,False)
 			m.set_ydata(J_o2*1E10)
 			l.set_ydata(o3/1E12)
 			n.set_ydata(J_o3*1E3)
@@ -90,61 +92,68 @@ if steadystate==True:
 		linoxyoz(nlevs,h_max,h_min,H,o2_c,o3_c,T,sol,sol_bin_width)
 
 if new_numerics==True:
-	o2_running=0
-	o3_running=0
-        d1defs=np.genfromtxt("species.dat",dtype='str',skiprows=2)
-        bimol=np.genfromtxt("bimol.dat",dtype='str',skiprows=2)
-        photo=np.genfromtxt("photol.dat",dtype='str',skiprows=2)
-        nspec=len(d1defs[:,0])
-	d1=d1_init(d1defs,nlevs,ratio)
-	M=d1[np.where(d1defs=='M')[0][0],:]
-        d3=np.zeros([nspec,nlevs])
-	for i in range(nlevs):
+ o2_running=0
+ o3_running=0
+ d1defs=np.genfromtxt("species.dat",dtype='str',skiprows=2)
+ bimol=np.genfromtxt("bimol.dat",dtype='str',skiprows=2)
+ photo=np.genfromtxt("photol.dat",dtype='str',skiprows=2)
+ nspec=len(d1defs[:,0])
+ d1=d1_init(d1defs,nlevs,ratio)
+ M=d1[np.where(d1defs=='M')[0][0],:]
+ d3=np.zeros([nspec,nlevs])
+ for i in range(nlevs):
 # Create dictionary of rates
-                rates={}
-                for a in range(len(bimol)):
-                        rates[bimol[a,2]]=k(bimol[a,2],T[i],M[i])
-                I=od_chems(o2_running,o3_running,o2_c,o3_c,sol)
-                for a in range(len(photo)):
-                        rates[photo[a,1]]=j(I,o2_c,o3_c,photo[a,1],sol_bin_width)
+  rates={}
+  for a in range(len(bimol)):
+   rates[bimol[a,2]]=k(bimol[a,2],T[i],M[i])
+  I=od_chems(o2_running,o3_running,o2_c,o3_c,sol)
+  for a in range(len(photo)):
+   rates[photo[a,1]]=j(I,o2_c,o3_c,photo[a,1],sol_bin_width)
 # Define function for chemical tendencies
-		def f(y, t):
-			g=np.empty(nspec)
+  def f(y, t):
+   g=np.empty(nspec)
 # Loop through chemical species
-			for spec in range(nspec):
+   for spec in range(nspec):
 # Set to zero if species constant, e.g. M, O2
-				if d1defs[spec,1]=='n':
-					g[spec]=0.0
-				else:
-					g[spec]=0.0
+    if d1defs[spec,1]=='n':
+     g[spec]=0.0
+    else:
+     g[spec]=0.0
 # Add chemical tendency for a bimolecular rxn
 # np.where(bimol==d1defs[spec,0])[:][0].shape[0] is the number of times spec appears in bimol	
-					for bi in range(np.where(bimol==d1defs[spec,0])[:][0].shape[0]):
-						rxno=np.where(bimol==d1defs[spec,0])[0][bi]
-						if np.where(bimol==d1defs[spec,0])[1][bi]<2:
-							g[spec]-=y[np.where(d1defs==bimol[rxno,0])[0][0]]*y[np.where(d1defs==bimol[rxno,1])[0][0]]*rates[bimol[rxno,2]]
-						else:
-                                                        g[spec]+=y[np.where(d1defs==bimol[rxno,0])[0][0]]*y[np.where(d1defs==bimol[rxno,1])[0][0]]*rates[bimol[rxno,2]]
-
-					for pho in range(np.where(photo==d1defs[spec,0])[:][0].shape[0]):
-						rxno=np.where(photo==d1defs[spec,0])[0][pho]
-						if np.where(photo==d1defs[spec,0])[1][pho]<1:
-							g[spec]-=y[np.where(d1defs==photo[rxno,0])[0][0]]*rates[photo[rxno,1]]
-						else:
-                                                        g[spec]+=y[np.where(d1defs==photo[rxno,0])[0][0]]*rates[photo[rxno,1]]
-			return g
-		#set up times
-		time_tot=7200
-		t=np.linspace(0,time_tot,time_tot/3600)
-		#solution provides a time series, take the final value
-		soln=odeint(f,d1[:,i],t)
-		soln=soln[len(t)-1,:]
-		d3[np.where(d1defs=='O3')[0][0],i]=soln[1]
-                d3[np.where(d1defs=='O')[0][0],i]=soln[2]
-		o3_running=o3_running+d3[np.where(d1defs=='O3')[0][0],i]*box_h
-		o2_running=o2_running+d1[np.where(d1defs=='O2')[0][0],i]*box_h
-	#print d3[np.where(d1defs=='O3')[0][0],:]
-	#print o3_init
-	plt.plot(heights,(d1[np.where(d1defs=='O3')[0][0],:]-d3[np.where(d1defs=='O3')[0][0],:])/d1[np.where(d1defs=='O3')[0][0],:])
-	plt.savefig('new_numerics.png')
-	print d3[np.where(d1defs=='H2O')[0][0],:]
+     for bi in range(np.where(bimol==d1defs[spec,0])[:][0].shape[0]):
+      rxno=np.where(bimol==d1defs[spec,0])[0][bi]
+      if np.where(bimol==d1defs[spec,0])[1][bi]<2:
+# If appears on LHS of eqn, results in loss
+       g[spec]-=y[np.where(d1defs==bimol[rxno,0])[0][0]]\
+                *y[np.where(d1defs==bimol[rxno,1])[0][0]]\
+                *rates[bimol[rxno,2]]
+      else:
+# If appears on RHS of eqn, results in production
+       g[spec]+=y[np.where(d1defs==bimol[rxno,0])[0][0]]\
+                *y[np.where(d1defs==bimol[rxno,1])[0][0]]\
+                *rates[bimol[rxno,2]]
+     for pho in range(np.where(photo==d1defs[spec,0])[:][0].shape[0]):
+      rxno=np.where(photo==d1defs[spec,0])[0][pho]
+      if np.where(photo==d1defs[spec,0])[1][pho]<1:
+       g[spec]-=y[np.where(d1defs==photo[rxno,0])[0][0]]\
+                *rates[photo[rxno,1]]
+      else:
+       g[spec]+=y[np.where(d1defs==photo[rxno,0])[0][0]]\
+                *rates[photo[rxno,1]]
+   return g
+# Set up times
+  time_tot=7200
+  t=np.linspace(0,time_tot,time_tot/3600)
+#solution provides a time series, take the final value
+  soln=odeint(f,d1[:,i],t)
+  soln=soln[len(t)-1,:]
+  d3[np.where(d1defs=='O3')[0][0],i]=soln[1]
+  d3[np.where(d1defs=='O')[0][0],i]=soln[2]
+  o3_running=o3_running+d3[np.where(d1defs=='O3')[0][0],i]*box_h
+  o2_running=o2_running+d1[np.where(d1defs=='O2')[0][0],i]*box_h
+ plt.plot(heights,(d1[np.where(d1defs=='O3')[0][0],:]\
+                  -d3[np.where(d1defs=='O3')[0][0],:])\
+                  /d1[np.where(d1defs=='O3')[0][0],:])
+ plt.savefig('new_numerics.png')
+ print d3[np.where(d1defs=='H2O')[0][0],:]
