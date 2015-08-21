5bc479b2aa0a2d3db235494bc30767caffd772d0
dcw32 <dcw32@cam.ac.uk>
2015-08-21 17:35:57 +0100

    NetCDF

--- netcdf/initialise.py
+++ netcdf/initialise.py
@@ -1,18 +1,20 @@
 from scipy import interpolate
 import numpy as np
 from netCDF4 import Dataset
-species='ClO'
-dataset=Dataset(species+'.nc','w',format='NETCDF4')
+species='N2O'
+dataset=Dataset('initial.nc','r+',format='NETCDF4')
 height=np.linspace(80,0,81)
-dataset.createDimension('height',len(height))
-heights=dataset.createVariable('height',np.float32,('height',))
-heights[:]=height
-spec=dataset.createVariable(species,np.float32,('height',))
+#dataset.createDimension('height',len(height))
+#heights=dataset.createVariable('height',np.float32,('height',))
+#heights[:]=height
+spec=dataset.variables[species]
 marray=Dataset('M.nc','r')
 m=marray.variables['M'][:]
 m=m[::-1]
-array_spec=np.zeros(81)
-for i in range(81):
-	array_spec[i]=3E-9*m[i]
+array_spec=np.ones(81)
+for i in range(10,50):
+	array_spec[i]=1E-9*(320-(i-10)*8)*m[i]
+for i in range(0,10):
+	array_spec[i]=320E-9*m[i]
 array_spec=array_spec[::-1]
 spec[:]=array_spec
